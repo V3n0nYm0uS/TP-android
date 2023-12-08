160b1db1a873aee43ea7cc6b12110d1838746471
Loan Dumenoir <loan.dumenoir@gmail.com>
2023-12-08 12:04:37 +0100

    Ajout de produits

--- app/src/main/java/fr/unilasalle/tdandroid/MainActivity.kt
+++ app/src/main/java/fr/unilasalle/tdandroid/MainActivity.kt
@@ -10,37 +10,45 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import fr.unilasalle.tdandroid.ui.theme.MyApplicationTheme
+import layout.ProductAdapter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        setContentView(R.layout.mainview)
+        val recyclerView: RecyclerView = findViewById(R.id.my_products)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        val productList = listOf(
+            Product(
+                id = 1,
+                title = "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+                price = 109.95,
+                description = "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+                category = "men's clothing",
+                image = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+                rating = Rating(rate = 3.9, count = 120)
+            ),
+            Product(
+                id = 2,
+                title = "Mens Casual Premium Slim Fit T-Shirts",
+                price = 22.3,
+                description = "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+                category = "men's clothing",
+                image = "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+                rating = Rating(rate = 4.1, count = 259)
+            )
+        )
+
+        val adapter = ProductAdapter(productList)
+        recyclerView.adapter = adapter
+
+
     }
 }
---
+++ app/src/main/java/fr/unilasalle/tdandroid/ProductAdapter.kt
@@ -0,0 +1,47 @@
+package layout
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import fr.unilasalle.tdandroid.Product
+import fr.unilasalle.tdandroid.R
+
+class ProductAdapter(private val productList: List<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_product, parent, false)
+        return ProductViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int){
+        val item = productList[position]
+        holder.viewId.text = "${item.id}"
+        holder.viewTitle.text = "${item.title}"
+        holder.viewPrice.text = "${item.price}"
+        holder.viewDescription.text = "${item.description}"
+        holder.viewCategory.text = "${item.category}"
+        val product = productList[position]
+        holder.bind(product)
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+
+    inner class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val viewId: TextView = itemView.findViewById(R.id.id)
+        val viewTitle: TextView = itemView.findViewById(R.id.title)
+        val viewPrice: TextView = itemView.findViewById(R.id.price)
+        val viewDescription: TextView = itemView.findViewById(R.id.description)
+        val viewCategory: TextView = itemView.findViewById(R.id.category)
+        // TODO --Image for --val viewImage: TextView = itemView.findViewById(R.id.image)
+        // TODO -- recycler for --val viewRating:
+
+        fun bind(product: Product){
+            //
+        }
+    }
+}
