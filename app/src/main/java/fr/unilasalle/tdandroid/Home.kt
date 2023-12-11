2b97c5086887161e4a7e6a203b9d76d4d74a17fe
Honoré <honoreishimwe6@gmail.com>
2023-12-11 20:04:03 +0100

    changes to the UI responsivness

--- app/src/main/java/fr/unilasalle/tdandroid/Home.kt
+++ app/src/main/java/fr/unilasalle/tdandroid/Home.kt
@@ -1,11 +1,26 @@
 package fr.unilasalle.tdandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val spanCount = calculateSpanCount()
+        val layoutManager = GridLayoutManager(this, spanCount)
+        recyclerView.layoutManager = layoutManager
+    }
+
+    private fun calculateSpanCount(): Int {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val screenWidthDp = displayMetrics.widthPixels / displayMetrics.density
+        val columnWidthDp = 180
+        return Math.max(1, (screenWidthDp / columnWidthDp).toInt())
     }
 }
